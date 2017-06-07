713ded05036a2f4ed83c1c04619016a05aa57e8a
Yangguoxin <825656776@qq.com>
2017-06-07 18:48:33 +0800

    Load face image when enter Gallery

--- apps/gallery/js/app.js
+++ apps/gallery/js/app.js
@@ -38,6 +38,7 @@ $(document).ready(function () {
 					.then(function () {
 						Gallery.getFiles(currentLocation).then(function () {
 							window.onhashchange();
+                            Gallery.getSearch();
 						});
 					});
 			});
